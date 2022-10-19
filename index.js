3f46782ddd13a920614c70dcb326d963926483ad
Gabriel Barimboim <gdbarimboim@gmail.com>
2022-10-19 14:49:37 -0300

    ready

--- index.js
+++ index.js
@@ -6,34 +6,42 @@ fetch(endpoint)
   .then((blob) => blob.json())
   .then((data) => cities.push(...data));
 
-function findmatches (wordMatch, cities) {
-  return cities.filter(place => {
-    const regex = new RegExp(wordMatch, "gi")
-    return place.city.match(regex) || place.state.match(regex)
-  })
+function findmatches(wordMatch, cities) {
+  return cities.filter((place) => {
+    const regex = new RegExp(wordMatch, "gi");
+    return place.city.match(regex) || place.state.match(regex);
+  });
 }
 function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 function displayMatches() {
-  const matchArray = findmatches(this.value, cities)
-  const html = matchArray.map(place => {
-    const regex = new RegExp(this.value, "gi")
-    const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`)
-    const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`)
-    return `
+  const matchArray = findmatches(this.value, cities);
+  const html = matchArray
+    .map((place) => {
+      const regex = new RegExp(this.value, "gi");
+      const cityName = place.city.replace(
+        regex,
+        `<span class="hl">${this.value.toUpperCase()}</span>`
+      );
+      const stateName = place.state.replace(
+        regex,
+        `<span class="hl">${this.value.toUpperCase()}</span>`
+      );
+      return `
     <li>
       <span class="name">${cityName}, ${stateName}</span>
       <span class="population">${numberWithCommas(place.population)}</span>
     </li>
-    `
-  }).join("")
+    `;
+    })
+    .join("");
   suggestions.innerHTML = html;
 }
 
-const sercher = document.querySelector(".searcher")
-const suggestions = document.querySelector(".suggestions")
+const sercher = document.querySelector(".searcher");
+const suggestions = document.querySelector(".suggestions");
 
-sercher.addEventListener("change", displayMatches)
-sercher.addEventListener("keyup", displayMatches)
+sercher.addEventListener("change", displayMatches);
+sercher.addEventListener("keyup", displayMatches);
